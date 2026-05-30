310f95938f8cdb4c0b79f96c82e7243ead38ba31
Neo-Ansh <amishra4416@gmail.com>
2026-05-30 17:00:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV != "production") {
   require("dotenv").config();
 }
-
+const { cloudinary } = require("./cloudConfig");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");

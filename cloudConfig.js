212a0d35a41d4763e27c3957d49f5c3dac47ae20
Neo-Ansh <amishra4416@gmail.com>
2026-05-30 16:38:44 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -19,3 +19,11 @@ module.exports = {
     cloudinary,
     storage
 };
+
+//temporary change to understand deploy errors
+console.log("CLOUD_NAME =", process.env.CLOUD_NAME);
+console.log("CLOUD_API_KEY =", process.env.CLOUD_API_KEY);
+console.log(
+  "CLOUD_API_SECRET =",
+  process.env.CLOUD_API_SECRET ? "Loaded" : "Missing"
+);
